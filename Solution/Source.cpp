e40015cf48059131440efd0cb2775555744c9116
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2017-11-14 09:49:31 +0000

    Add List class, rewrite algorithmn for List

--- Solution/Source.cpp
+++ Solution/Source.cpp
@@ -1,29 +1,111 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<int> mergeSort(vector<int> intList)
+class List
 {
-	if (intList.size() == 1)
-		return intList;
+private:
+	int intSize;
+	int* intArray;
+public:
+	List() {};
+	List(const int intSIZE) { intSize = intSIZE; intArray = new int[intSIZE]; };
+	~List() { delete intArray; }
 
-	vector<int>::iterator iterMiddle = intList.begin() + (intList.size() / 2);
+	void set(int intIndex, int intValue) { intArray[intIndex] = intValue; }
+	int get(int intIndex) { return intArray[intIndex]; };
+	int size() { return intSize; };
+};
 
-	vector<int> intLeft(intList.begin(), iterMiddle);
-	vector<int> intRight(iterMiddle, intList.end());
+List merge(List& lstLeft, List& lstRight)
+{
+	List lstResult(lstLeft.size() * 2);
+	int intCount = 0, intLeft = 0, intRight = 0;
 
-	intLeft = mergeSort(intLeft);
-	intRight = mergeSort(intRight);
+	while (intLeft < lstLeft.size() && intRight < lstRight.size())
+	{
+		if (lstLeft.get(intLeft) < lstRight.get(intRight))
+		{
+			lstResult.set(intCount, lstLeft.get(intLeft));
+			intCount++;
+			intLeft++;
+		}
+		else
+		{
+			lstResult.set(intCount, lstRight.get(intRight));
+			intCount++;
+			intRight++;
+		}
+	}
 
-	return merge(intLeft, intRight);
+	while (intLeft < lstLeft.size())
+	{
+		lstResult.set(intCount, lstLeft.get(intLeft));
+		intCount++;
+		intLeft++;
+	}
+
+	while (intRight < lstRight.size())
+	{
+		lstResult.set(intCount, lstRight.get(intRight));
+		intCount++;
+		intRight++;
+	}
+
+	return lstResult;
+}
+List mergeSort(List lstNumbers)
+{
+	if (lstNumbers.size() == 1)
+		return lstNumbers;
+
+	int intMiddle = lstNumbers.size() / 2;
+
+	List lstLeft(intMiddle), lstRight(intMiddle);
+
+	for (int intIndex = 0; intIndex < intMiddle; intIndex++)
+	{
+		lstLeft.set(intIndex, lstNumbers.get(intIndex));
+		lstRight.set(intIndex, lstNumbers.get(intIndex + intMiddle));
+	}
+
+	lstLeft = mergeSort(lstLeft);
+	lstRight = mergeSort(lstRight);
+
+	return merge(lstLeft, lstRight);
 }
 
-vector<int> merge(const vector<int>& intLeft, const vector<int>& intRight)
+bool program()
 {
 
-}
+	List lstNumbers(8);
+	lstNumbers.set(0, 9);
+	lstNumbers.set(1, 2);
+	lstNumbers.set(2, 7);
+	lstNumbers.set(4, 1);
+	lstNumbers.set(5, 4);
+	lstNumbers.set(6, 5);
+	lstNumbers.set(7, 8);
 
+	lstNumbers = mergeSort(lstNumbers);
+	for (int intIndex = 0; intIndex < 8; intIndex++)
+		cout << lstNumbers.get(intIndex) << " ";
+
+	char charInput;
+	do
+	{
+		cout << "Continue (y/n)? ";
+		cin >> charInput;
+	} while (charInput != 'y' || charInput != 'n');
+
+	if (charInput == 'y')
+		return false;
+	else if (charInput == 'n')
+		return true;
+}
 int main(void)
 {
+	while (program());
 	return 0;
 }
