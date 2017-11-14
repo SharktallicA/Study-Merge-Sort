90225fee42717106b7556b1baf0376760d57428d
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2017-11-14 08:46:26 +0000

    Add vector-based merge sort method

--- Solution/Source.cpp
+++ Solution/Source.cpp
@@ -1,4 +1,27 @@
+#include <vector>
+
 using namespace std;
+
+vector<int> mergeSort(vector<int> intList)
+{
+	if (intList.size() == 1)
+		return intList;
+
+	vector<int>::iterator iterMiddle = intList.begin() + (intList.size() / 2);
+
+	vector<int> intLeft(intList.begin(), iterMiddle);
+	vector<int> intRight(iterMiddle, intList.end());
+
+	intLeft = mergeSort(intLeft);
+	intRight = mergeSort(intRight);
+
+	return merge(intLeft, intRight);
+}
+
+vector<int> merge(const vector<int>& intLeft, const vector<int>& intRight)
+{
+
+}
 
 int main(void)
 {
